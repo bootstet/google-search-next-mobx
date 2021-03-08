485af309e162ecf0873779672c03ec9d4835de2b
p_youzcchen <chenyouzeng@chenyouzengdeMacBook-Air.local>
2021-03-09 01:50:06 +0800

    init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,37 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import UrlList from './urlList'
+import UrlListStore from './store/urlNetListStore'
+import UrlItemStore from './store/urlNetItemStore'
+
+const urlListPageStore = new UrlListStore()
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>google search page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <img src='./images/google_logo.svg'/>
         </div>
+        <div className={styles.grid}>
+          <input className="search"/>
+          <style jsx>{`
+            .search {
+              width: 500px;
+              height: 40px;
+              padding: 0 40px;
+              border-radius: 20px;
+              border: 1px solid #333;
+            }
+          `}</style>
+        </div>
+        <UrlList urlListPageStore={urlListPageStore} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
